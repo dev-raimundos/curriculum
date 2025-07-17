d73701e3a97965842e40669fa344878fd530ab80
dev-raimundos <devraimundos@outlook.com>
2025-07-17 00:16:10 -0300

    FEAT: About component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { Header } from './components/header/header';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, NgOptimizedImage, Header ],
+  imports: [ RouterOutlet, NgOptimizedImage, Header, About ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
