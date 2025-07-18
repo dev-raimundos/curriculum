85094e91cb720f09fe3cff2713b6ee9b09448447
dev-raimundos <devraimundos@outlook.com>
2025-07-18 11:43:06 -0300

    FIX: Rearranjando a lógica de de design do Mobile

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { Header } from './components/header/header';
 import { About } from './components/about/about';
+import { WorkExperience } from './components/work-experience/work-experience';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, NgOptimizedImage, Header, About ],
+  imports: [ RouterOutlet, NgOptimizedImage, Header, About, WorkExperience ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
