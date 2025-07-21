1def88e00935dbd353d58b0f64044a90bc05ff1f
dev-raimundos <devraimundos@outlook.com>
2025-07-20 21:42:18 -0300

    FIX: Adicionando secção de artigos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
 import { Header } from './components/header/header';
 import { About } from './components/about/about';
 import { WorkExperience } from './components/work-experience/work-experience';
 import { Education } from './components/education/education';
 import { Contact } from './components/contact/contact';
+import { Writing } from './components/writing/writing';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, NgOptimizedImage, Header, About, WorkExperience, Education, Contact ],
+  imports: [
+    Header,
+    About,
+    WorkExperience,
+    Education,
+    Contact,
+    Writing
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
